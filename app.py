4855cbb065a6f4cf96dd0e2d7139feefa5191a8c
rosiefn8282 <rose.pink.fn82@gmail.com>
2025-05-30 18:18:28 +0330

    Add files via upload

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-# کد اصلی اپلیکیشن Flask اینجا قرار می‌گیرد
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "سلام، برنامه Flask شما روی Render اجرا شده است!"
+
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
